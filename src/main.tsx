092ae7e9dff0568c1e8ab2109a82f19a06a53d10
Eugene Klimenkov <eugene.klimenkov@yournextagency.com>
2025-08-28 22:42:02 +0300

    [F-8] add rules if it's tg

--- src/main.tsx
+++ src/main.tsx
@@ -3,19 +3,21 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./app";
 import { BrowserRouter } from "react-router-dom";
-import { init, viewport } from "@telegram-apps/sdk";
+import { init, isTMA, viewport } from "@telegram-apps/sdk";
 
-init();
-console.log('connect telegram...')
-if (viewport.mount.isAvailable()) {
-  viewport
-    .mount()
-    .then(() => {
-      return viewport.requestFullscreen.ifAvailable();
-    })
-    .catch((error) => {
-      console.warn("Failed to mount viewport:", error);
-    });
+if (isTMA()) {
+  init();
+  console.log('connect telegram...')
+  if (viewport.mount.isAvailable()) {
+    viewport
+      .mount()
+      .then(() => {
+        return viewport.requestFullscreen.ifAvailable();
+      })
+      .catch((error) => {
+        console.warn("Failed to mount viewport:", error);
+      });
+  }
 }
 
 createRoot(document.getElementById("root")!).render(
